5823c1e039ef88cf7b15ad8ff70a380a99ee5993
fressysharon <fressysharon@student.ub.ac.id>
2025-09-10 17:20:08 +0700

    Create laprak01.java

---
+++ laprak01.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class laprak01 {
+
+     public static void main(String[] args) {
+          Scanner input = new Scanner(System.in);
+          int a, b;
+          int penjumlahan, pengurangan, perkalian;
+          double pembagian;
+
+          // input
+          System.out.print("Masukkan operator pertama : ");
+          a = input.nextInt();
+          System.out.print("Masukkan operator kedua   : ");
+          b = input.nextInt();
+
+          // hitung/
+          penjumlahan = a + b;
+          pengurangan = a - b;
+          perkalian = a * b;
+          pembagian = (double) a / b;
+
+          // output
+          System.out.println("Hasil penjumlahan : " + penjumlahan);
+          System.out.println("Hasil pengurangan : " + pengurangan);
+          System.out.println("Hasil perkalian   : " + perkalian);
+          System.out.println("Hasil pembagian   : " + pembagian);
+     }
+}
